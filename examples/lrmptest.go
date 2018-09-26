19bae72de2b922c004bbf1ba88cfb47c01279abb
robaho <rengels@ix.netcom.com>
2018-09-26 00:25:20 -0500

    log packets as strings

--- examples/lrmptest.go
+++ examples/lrmptest.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/robaho/lrmp"
 	"log"
 	"sync"
 )
 
+type handler struct {
+}
+
+func (handler) ProcessData(p *lrmp.Packet) {
+	fmt.Println("got a packet", string(p.GetData()))
+}
+
+func (handler) ProcessEvent(event int, data interface{}) {
+	fmt.Println("got an event", event, data)
+}
+
 func main() {
 	wg := sync.WaitGroup{}
 
-	profile := lrmp.Profile{}
+	profile := lrmp.NewProfile()
+	profile.Handler = new(handler)
 
-	lrmp, err := lrmp.NewLrmp("225.0.0.100", 6000, 0, "en0", profile)
+	lrmp, err := lrmp.NewLrmp("225.0.0.100", 6000, 0, "en0", *profile)
 	if err != nil {
 		log.Fatal(err)
 	}
